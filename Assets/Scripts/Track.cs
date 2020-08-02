42c452c08aba7dcaba86996a6d62f51228982fa0
Caio Rosisca <caio.rosisca@hotmail.com>
2020-08-02 19:31:31 -0300

    track data and view

---
+++ Assets/Scripts/Track.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Beats
+{
+    //Model
+    [CreateAssetMenu(menuName = "Beats/New Track", fileName = "New Beats Track.asset")]
+    public class Track : ScriptableObject
+    {
+        [Header("Playback Settings")]
+
+        [Tooltip("# of beats per minute")]
+        [Range(30, 360)]
+        public int bpm = 120;
+
+        [HideInInspector]
+        public List<int> beats;
+
+        //Shouldn't have data generation method inside the model itself
+        public static int inputs = 4;
+
+        [Header("Random Settings")]
+
+        [Tooltip("# of preroll (empty) beats")]
+        [Range(0, 10)]
+        [SerializeField]
+        int _preroll = 10;
+
+        [Tooltip("Minimum # of beats per block")]
+        [Range(1, 20)]
+        [SerializeField]
+        int _minBlock = 2;
+
+        [Tooltip ("Maximim # of beats per block")]
+        [Range(1, 20)]
+        [SerializeField]
+        int _maxBlock = 5;
+
+        [Tooltip("Minimum # of empty beats between blocks")]
+        [Range(1, 20)]
+        [SerializeField]
+        int _minInterval = 1;
+
+        [Tooltip("Maximum # of empty beats between blocks")]
+        [Range(1, 20)]
+        [SerializeField]
+        int _maxInterval = 2;
+
+        [Tooltip("# of beats blocks")]
+        [Range(1, 20)]
+        [SerializeField]
+        int _blocks = 10;
+
+        public void Randomize()
+        {
+            beats = new List<int>();
+
+            //Preroll
+            for (int i = 0; i < _preroll; i++)
+                beats.Add(-1);
+
+            //Blocks
+            for (int block = 0; block < _blocks; block++)
+            {
+                //Beats
+                int blockLength = Random.Range(_minBlock, _maxBlock + 1);
+                for (int j = 0; j < blockLength; j++)
+                {
+                    int beat = Random.Range(0, inputs);
+                    beats.Add(beat);
+                }
+
+                //Dont add interval on  last block
+                if (block == _blocks - 1)
+                    break;
+
+                //Interval
+                int intervalLength = Random.Range(_minInterval, _maxInterval + 1);
+                for (int j = 0; j < intervalLength; j++)
+                    beats.Add(-1);
+            }
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/TrackView.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Beats
+{
+    [RequireComponent(typeof(VerticalLayoutGroup))]
+    [RequireComponent(typeof(ContentSizeFitter))]
+    [RequireComponent(typeof(RectTransform))]
+    public class TrackView : MonoBehaviour
+    {
+        [SerializeField]
+        Track _track;
+
+        [SerializeField]
+        RectTransform _left;
+
+        [SerializeField]
+        RectTransform _right;
+
+        [SerializeField]
+        RectTransform _up;
+
+        [SerializeField]
+        RectTransform _down;
+
+        [SerializeField]
+        RectTransform _empty;
+
+        RectTransform _rectTransform;
+
+        Vector2 _position;
+        public float position
+        {
+            get { return _position.y; }
+            set
+            {
+                if (value != _position.y)
+                {
+                    _position.y = value;
+                    _rectTransform.anchoredPosition = _position;
+                }
+            }
+        }
+
+
+        public void Init(Track track)
+        {
+            _rectTransform = (RectTransform) transform;
+            _position = _rectTransform.anchoredPosition;
+
+            foreach (int beat in track.beats)
+            {
+                GameObject go;
+                switch (beat)
+                {
+                    case 0:
+                        go = _left.gameObject;
+                        break;
+                    case 1:
+                        go = _right.gameObject;
+                        break;
+                    case 2:
+                        go = _up.gameObject;
+                        break;
+                    case 3:
+                        go = _down.gameObject;
+                        break;
+                    default:
+                        go = _empty.gameObject;
+                            break;
+                }
+
+                Transform view = GameObject.Instantiate(go, transform).transform;
+                view.SetAsFirstSibling();
+            }
+
+        }
+
+        void Start()
+        {
+            Init(_track);
+        }
+
+        void Update()
+        {
+            position -= Time.deltaTime * 100;
+        }
+    }
+}
